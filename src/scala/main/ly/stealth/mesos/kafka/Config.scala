96e3220f5f83e36b95501d5ed39262547e79db15
Di Shang <shangdi@au1.ibm.com>
2017-03-02 12:04:45 +1100

    change new scheduler version string to be compatible with the 0.9 version to enable rollback

--- src/scala/main/ly/stealth/mesos/kafka/Config.scala
+++ src/scala/main/ly/stealth/mesos/kafka/Config.scala
@@ -24,7 +24,7 @@ import ly.stealth.mesos.kafka.Util.BindAddress
 import net.elodina.mesos.util.{Period, Version}
 
 object SchedulerVersion {
-  val value = "0.10.1.1-SNAPSHOT"
+  val value = "0.10.1.1"
 }
 
 object Config {
