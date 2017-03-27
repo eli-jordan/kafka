14d17f3247fb72f23e629164bf429b4ec7f169c4
Eli Jordan <elias.k.jordan@gmail.com>
2017-03-27 17:41:02 +1100

    add artifactory publish

--- src/scala/main/ly/stealth/mesos/kafka/Config.scala
+++ src/scala/main/ly/stealth/mesos/kafka/Config.scala
@@ -24,7 +24,7 @@ import ly.stealth.mesos.kafka.Util.BindAddress
 import net.elodina.mesos.util.{Period, Version}
 
 object SchedulerVersion {
-  val value = "0.9.5.5"
+  val value = "0.9.5.6"
 }
 
 object Config {
